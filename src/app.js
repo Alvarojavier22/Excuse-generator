f3b4d77cf3504f594a920f70f3c2460e103b5e5b
Alvarojavier22 <alvarojavierchagas@hotmail.com>
2022-10-05 12:59:11 -0300

    Boton funcionando

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo from the console! ");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["Mi", "El"];
+  let subject = ["perro", "gato", "loro", "chofer"];
+  let action = ["comió", "rompió", "quebró"];
+  let possesion = ["la tarea", "el auto", "un zapato"];
+  let where = ["en mi casa", "en la calle", "en el patio"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possesionIndex = Math.floor(Math.random() * possesion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possesion[possesionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
